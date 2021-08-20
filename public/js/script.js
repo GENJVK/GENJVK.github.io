dcbd88524518631c81550867545c5a6e1df6f039
vic-torylf <victoryipstu@gmail.com>
2021-08-20 11:46:41 +0800

    "updated"

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,5 @@
 // side-menu
 let isOpen = false;
-
 function menuToggle() {
     if (isOpen === false) {
         document.getElementById('sideMenu').style.width = "200px";
@@ -12,9 +11,92 @@ function menuToggle() {
         isOpen = false;
     }
 }
-
 document.getElementById('menu-button').addEventListener('click', menuToggle);
 
+// current time
+function startTime() {
+    let today = new Date();
+    let strDate = document.getElementById("datetime");
+    strDate.innerHTML = ("  " + " " + today.getDate() + ",    " + today.getFullYear());
+
+    let n_month = (today.getMonth() + 1);
+    switch (n_month) {
+        case 0:
+            strDate.innerHTML = " December " + strDate.innerHTML
+            break;
+        case 1:
+            strDate.innerHTML = " January " + strDate.innerHTML
+            break;
+        case 2:
+            strDate.innerHTML = " February " + strDate.innerHTML
+            break;
+        case 3:
+            strDate.innerHTML = " March " + strDate.innerHTML
+            break;
+        case 4:
+            strDate.innerHTML = " April " + strDate.innerHTML
+            break;
+        case 5:
+            strDate.innerHTML = " May " + strDate.innerHTML
+            break;
+        case 6:
+            strDate.innerHTML = " June " + strDate.innerHTML
+            break;
+        case 7:
+            strDate.innerHTML = " July " + strDate.innerHTML
+            break;
+        case 8:
+            strDate.innerHTML = " August " + strDate.innerHTML
+            break;
+        case 9:
+            strDate.innerHTML = " September " + strDate.innerHTML
+            break;
+        case 10:
+            strDate.innerHTML = " October " + strDate.innerHTML
+            break;
+        case 11:
+            strDate.innerHTML = " November " + strDate.innerHTML
+            break;
+        case 12:
+            strDate.innerHTML = " December " + strDate.innerHTML
+            break;
+    }
+    let n_day = today.getDay();
+    switch (n_day) {
+        case 0:
+            strDate.innerHTML = " Sunday, " + strDate.innerHTML
+            break;
+        case 1:
+            strDate.innerHTML = " Monday, " + strDate.innerHTML
+            break;
+        case 2:
+            strDate.innerHTML = " Tuesday, " + strDate.innerHTML
+            break;
+        case 3:
+            strDate.innerHTML = " Wednesday, " + strDate.innerHTML
+            break;
+        case 4:
+            strDate.innerHTML = " Thursday, " + strDate.innerHTML
+            break;
+        case 5:
+            strDate.innerHTML = " Friday, " + strDate.innerHTML
+            break;
+        case 6:
+            strDate.innerHTML = " Saturday, " + strDate.innerHTML
+            break;
+        case 7:
+            strDate.innerHTML = " Sunday, " + strDate.innerHTML
+            break;
+    }
+    function checkTime(i) {
+        if (i < 10) { i = "0" + i }
+        return i
+    }
+    
+}
+startTime()
+
+// show
 const showButton = document.querySelector('#show-button')
 async function showData() {
 
@@ -138,99 +220,70 @@ async function showData() {
 }
 showButton.addEventListener('click', showData)
 
+// add-task icon
 window.onload = function(){
     let school_r = document.getElementById('school_r');
     let life_r = document.getElementById('life_r');
-    let urgent_r = document.getElementById('urgent_r');
-    let job_r = document.getElementById('job_r');
-    let school_ronOff = true; //創造一個開關,布爾值，true為1，false為0
-    let life_ronOff = true; //創造一個開關,布爾值，true為1，false為0
-    let urgent_ronOff = true; //創造一個開關,布爾值，true為1，false為0
-    let job_ronOff = true; //創造一個開關,布爾值，true為1，false為0
+    let job_r= document.getElementById('job_r');
+    let school_ronOff = true;//創造一個開關,布爾值，true為1，false為0
+    let life_ronOff = true;//創造一個開關,布爾值，true為1，false為0
+    let job_ronOff = true;//創造一個開關,布爾值，true為1，false為0
     let chAnge = document.getElementById('add-task');
     let chAngeInput = document.getElementById('input_main');
-
-function startTime() {
-    let today = new Date();
-    let strDate = document.getElementById("datetime");
-    strDate.innerHTML = ("  " + " " + today.getDate() + ",    " + today.getFullYear());
-
-    let n_month = (today.getMonth() + 1);
-    switch (n_month) {
-        case 0:
-            strDate.innerHTML = " December " + strDate.innerHTML
-            break;
-        case 1:
-            strDate.innerHTML = " January " + strDate.innerHTML
-            break;
-        case 2:
-            strDate.innerHTML = " February " + strDate.innerHTML
-            break;
-        case 3:
-            strDate.innerHTML = " March " + strDate.innerHTML
-            break;
-        case 4:
-            strDate.innerHTML = " April " + strDate.innerHTML
-            break;
-        case 5:
-            strDate.innerHTML = " May " + strDate.innerHTML
-            break;
-        case 6:
-            strDate.innerHTML = " June " + strDate.innerHTML
-            break;
-        case 7:
-            strDate.innerHTML = " July " + strDate.innerHTML
-            break;
-        case 8:
-            strDate.innerHTML = " August " + strDate.innerHTML
-            break;
-        case 9:
-            strDate.innerHTML = " September " + strDate.innerHTML
-            break;
-        case 10:
-            strDate.innerHTML = " October " + strDate.innerHTML
-            break;
-        case 11:
-            strDate.innerHTML = " November " + strDate.innerHTML
-            break;
-        case 12:
-            strDate.innerHTML = " December " + strDate.innerHTML
-            break;
+ 
+    school_r.onclick = function(){
+        if(school_ronOff){//如果是真
+            school_r.src = './images/學校_Logo.png' ;//圖片路徑切換為圖片2
+            life_r.src = './images/文字框_生活_Logo.png';//圖片路徑切換為圖片1
+            job_r.src = './images/文字框_工作_Logo.png';//圖片路徑切換為圖片1
+            chAnge.style['background-color'] = '#f8d7da';
+            chAngeInput.style['background-color'] = '#f8d7da';
+            school_ronOff = false;//並且開關設為假
+            life_ronOff = true;//並且開關設為真
+            job_ronOff = true;//並且開關設為真
+        }else{//如果是假
+            school_r.src = './images/文字框_學校_Logo.png';//圖片路徑切換為圖片1
+            chAngeInput.style['background-color'] = '#e6e6e6';
+            chAnge.style['background-color'] = '#e6e6e6';
+            school_ronOff = true;//並且開關設為真
+        }
     }
-    let n_day = today.getDay();
-    switch (n_day) {
-        case 0:
-            strDate.innerHTML = " Sunday, " + strDate.innerHTML
-            break;
-        case 1:
-            strDate.innerHTML = " Monday, " + strDate.innerHTML
-            break;
-        case 2:
-            strDate.innerHTML = " Tuesday, " + strDate.innerHTML
-            break;
-        case 3:
-            strDate.innerHTML = " Wednesday, " + strDate.innerHTML
-            break;
-        case 4:
-            strDate.innerHTML = " Thursday, " + strDate.innerHTML
-            break;
-        case 5:
-            strDate.innerHTML = " Friday, " + strDate.innerHTML
-            break;
-        case 6:
-            strDate.innerHTML = " Saturday, " + strDate.innerHTML
-            break;
-        case 7:
-            strDate.innerHTML = " Sunday, " + strDate.innerHTML
-            break;
+    life_r.onclick = function(){
+        if(life_ronOff){//如果是真
+            life_r.src = './images/生活_Logo.png' ;//圖片路徑切換為圖片2
+            school_r.src = './images/文字框_學校_Logo.png' ;//圖片路徑切換為圖片2
+            job_r.src = './images/文字框_工作_Logo.png';//圖片路徑切換為圖片1
+            chAnge.style['background-color'] = '#b8daff';
+            chAngeInput.style['background-color'] = '#b8daff';
+            life_ronOff = false;//並且開關設為假
+            school_ronOff = true;//並且開關設為假
+            job_ronOff = true;//並且開關設為真
+        }else{//如果是假
+            life_r.src = './images/文字框_生活_Logo.png';//圖片路徑切換為圖片1
+            chAngeInput.style['background-color'] = '#e6e6e6';
+            chAnge.style['background-color'] = '#e6e6e6';
+            life_ronOff = true;//並且開關設為真
+        }
     }
-    function checkTime(i) {
-        if (i < 10) { i = "0" + i }
-        return i
+    job_r.onclick = function(){
+        if(job_ronOff){//如果是真
+            job_r.src = './images/工作_Logo.png' ;//圖片路徑切換為圖片2
+            life_r.src = './images/文字框_生活_Logo.png' ;//圖片路徑切換為圖片2
+            school_r.src = './images/文字框_學校_Logo.png' ;//圖片路徑切換為圖片2
+            chAnge.style['background-color'] = '#d1e7dd';
+            chAngeInput.style['background-color'] = '#d1e7dd';
+            job_ronOff = false;//並且開關設為假
+            life_ronOff = true;//並且開關設為假
+            school_ronOff = true;//並且開關設為假
+        }else{//如果是假
+            job_r.src = './images/文字框_工作_Logo.png';//圖片路徑切換為圖片1
+            chAngeInput.style['background-color'] = '#e6e6e6';
+            chAnge.style['background-color'] = '#e6e6e6';
+            job_ronOff = true;//並且開關設為真
+        }
     }
-    
 }
-startTime()
+
 
 // const fetchTodolist = document.querySelector('#showTodolist');
 // fetchTodolist.addEventListener('click', async (e) => {
@@ -283,4 +336,3 @@ document.querySelector('#add-task').addEventListener('submit', async (event) => 
         showData()
     }
 })
-}
