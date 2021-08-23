dec87752ca4c8000dfef8ec1553d4233aa9bb02b
PokeGuyChan <chenguan199530@gmail.com>
2021-08-23 15:51:35 +0800

    update calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,80 +1,157 @@
-// const console = require("console");
-// const { info } = require("console");
-
 // side-menu
-let isOpen = false;
+let isOpen = true;
 
 function menuToggle() {
     if (isOpen === false) {
-        document.getElementById('sideMenu').style.width = "200px";
-        document.getElementById("calendar").style.marginLeft = "200px";
+        document.getElementById('sideMenu').style.marginLeft = "0";
+        document.getElementById("container").style.marginLeft = "200px";
         isOpen = true;
     } else {
-        document.getElementById('sideMenu').style.width = "0";
-        document.getElementById("calendar").style.marginLeft = "0";
+        document.getElementById('sideMenu').style.marginLeft = "-210px";
+        document.getElementById("container").style.marginLeft = "0";
         isOpen = false;
     }
 }
 document.getElementById('menu-button').addEventListener('click', menuToggle);
 
+// 以下是calendar的coding！
 
-// calendar
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    const calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        dateClick: (info) => {
-            console.log(info.dateStr)
-            displayItemByDate(info.dateStr)
-        }
-    });
-    calendar.render();
-});
+const calendar = document.getElementById('calendar');
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
+// 定義 星期
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// 入資料
-const toDoListItem = [{
-    id: '1',
-    title: "do calender example",
-    assignedTo: 'Joseph',
-    status: 'No completed',
-    detail: 'ahahahaha',
-    date: '2021-08-21'
-}, {
-    id: '2',
-    title: "to do 1",
-    assignedTo: 'Anson',
-    status: 'No completed',
-    detail: 'ahahahaha',
-    date: '2021-08-21'
-}, {
-    id: '3',
-    title: "to do 2",
-    assignedTo: 'Victor',
-    status: 'No completed',
-    detail: 'ahahahaha',
-    date: '2021-08-21'
-}]
+function openModal(date) {
+    clicked = date;
 
-// 顯示data
-function displayItemByDate(date) {
-    const itemList = document.querySelector('.itemList')
+    const eventForDay = events.find(e => e.date === clicked);
 
-    const filterItems = toDoListItem.filter(item => item.date === date)
-
-    itemList.innerHTML = ''
-
-    for (const filterItem of filterItems) {
-        itemList.innerHTML += `
-        <div class="item">
-            <div class="itemHeader">
-                <p>title : ${filterItem.title}</p>
-                <p>assigned to : ${filterItem.assignedTo}</p>
-                <p>status: ${filterItem.status}</p>
-            </div>
-            <p>details : ${filterItem.detail}</p>
-        </div>
-        `
+    if (eventForDay) {
+        document.getElementById('eventText').innerText = eventForDay.title;
+        deleteEventModal.style.display = 'block';
+    } else {
+        newEventModal.style.display = 'block';
     }
 
+    backDrop.style.display = 'block';
 }
+// 製作一個日曆表
+function load() {
+    const dt = new Date();
+    // 切換月份
+    if (nav !== 0) {
+        dt.setMonth(new Date().getMonth() + nav);
+    }
+
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    });
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+    document.getElementById('monthDisplay').innerText =
+        `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+
+    console.log(`${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`)
+    calendar.innerHTML = '';
+
+    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+
+        const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+        if (i > paddingDays) {
+            daySquare.innerText = i - paddingDays;
+            const eventForDay = events.find(e => e.date === dayString);
+
+            if (i - paddingDays === day && nav === 0) {
+                daySquare.id = 'currentDay';
+            }
+
+            if (eventForDay) {
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
+        } else {
+            daySquare.classList.add('padding');
+        }
+
+        calendar.appendChild(daySquare);
+    }
+}
+
+function closeModal() {
+    eventTitleInput.classList.remove('error');
+    newEventModal.style.display = 'none';
+    deleteEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.value = '';
+    clicked = null;
+    load();
+}
+
+function saveEvent() {
+    if (eventTitleInput.value) {
+        eventTitleInput.classList.remove('error');
+
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value,
+        });
+
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+    } else {
+        eventTitleInput.classList.add('error');
+    }
+}
+
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
+}
+
+// 加入功能按鈕
+function initButtons() {
+    document.getElementById('nextButton').addEventListener('click', () => {
+        nav++;
+        load();
+    });
+
+    document.getElementById('backButton').addEventListener('click', () => {
+        nav--;
+        load();
+    });
+
+    document.getElementById('saveButton').addEventListener('click', saveEvent);
+    document.getElementById('cancelButton').addEventListener('click', closeModal);
+    document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+    document.getElementById('closeButton').addEventListener('click', closeModal);
+}
+
+initButtons();
+load();
+
+// 以上calendar的coding！！！
