f6cd395b9d5503f63295a035abcb58dae94f89d8
vic-torylf <victoryipstu@gmail.com>
2021-09-07 15:45:14 +0800

    "updated"

--- project-react/src/index.js
+++ project-react/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Home from '../src/pages/Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
