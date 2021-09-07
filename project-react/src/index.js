aa4fcf2f8ee6b57a9ad6bcc111678ed06a7a5255
vic-torylf <victoryipstu@gmail.com>
2021-09-07 17:10:25 +0800

    "suspend react"

--- project-react/src/index.js
+++ project-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from '../src/pages/Home';
+import Home from '../src/pages/js/Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
--- project-react/src/pages/js/Home.jsx
+++ project-react/src/pages/js/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './styles.css';
-import Navbar from '../components/Navbar';
+import '../css/styles.css';
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
 
 export default function Home() {
 
     return (
         <div>
+            <div className="header">
+                <Header />
+            </div>
             
+            <Navbar />
         </div>
     )
 }
